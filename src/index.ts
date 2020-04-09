dd0c1d6ff05b0d42d8624de0363801beef753dad
quavious <leenowon97@gmail.com>
2020-04-09 14:14:41 +0900

    class and interface

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
-interface Human {
-    name: string,
-    age : number,
-    gender: string
+class Human {
+    public name: string;
+    public age : number;
+    public gender : string;
+    constructor(name: string, age: number, gender?: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "Takeoff",
-    gender: "male",
-    age : 22
-}
+const person = new Human("Offset", 25, "male")
 
-const sayHi = (Human):string => {
+const sayHi = (person) => {
     return (`Hello ${person.name}, You are ${person.age}, you are ${person.gender}`)
 }
 
